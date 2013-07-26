09db7a26aab788ec29b69190189a12f3c11b2bda
Francesc Rosàs <francescrosasbosque@gmail.com>
2013-07-27 00:26:55 +0100

    Raise caching to 60 minutes

--- src/app.js
+++ src/app.js
@@ -31,7 +31,7 @@ app.get('/:id', function(request, response, next) {
     plus.userPosts(userId, styles, function(error, posts) {
         if (error) return next(error)
         response.header('Content-Type', 'text/xml')
-        response.header('Cache-Control', 'max-age=' + 55 * 60)
+        response.header('Cache-Control', 'max-age=' + 60 * 60)
         response.render('feed', {
             profileUrl: 'https://plus.google.com/' + userId,
             posts: posts,
