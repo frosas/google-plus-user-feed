207dcfd7d7020769552e10bd30e0b2a7a5d241ba
Francesc Rosàs <francescrosasbosque@gmail.com>
2014-01-25 22:15:02 +0000

    minAge 10 -> 20

--- src/app.js
+++ src/app.js
@@ -39,7 +39,7 @@ app.get('/:id', function(request, response, next) {
     }
 
     var getCacheMaxAge = function(posts) {
-        var minAge = 10 /* mins */ * 60
+        var minAge = 20 /* mins */ * 60
         var maxAge = 3 /* hours */ * 60 * 60
         return Math.max(minAge, Math.min(maxAge, getLapseSinceLastUpdate(posts)))
     }
