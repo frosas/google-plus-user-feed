f191f089d2fd2e598692f0ae603451a9b45190b2
Francesc Rosàs <francescrosasbosque@gmail.com>
2014-02-09 12:44:32 +0000

    Install New Relic

---
+++ newrelic.js
@@ -0,0 +1,13 @@
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+    app_name: ['Google+ User Feed'],
+    license_key: '020f82ad80dfc3b5139dd94734b421c4fce5c673',
+    logging: {
+        level: 'trace'
+    }
+};
--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+require('newrelic')
 require('sugar')
 
 var googlePlus = require('./google-plus'),
