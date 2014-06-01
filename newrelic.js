9f1db1959da43cd6d1324c28c2289f70fdc0fee1
Francesc Rosàs <francescrosasbosque@gmail.com>
2014-06-01 21:52:02 +0100

    New Relic trace -> info

--- newrelic.js
+++ newrelic.js
@@ -1,13 +1,12 @@
 /**
  * New Relic agent configuration.
  *
- * See lib/config.defaults.js in the agent distribution for a more complete
- * description of configuration variables and their potential values.
+ * See http://docs.newrelic.com/docs/nodejs/customizing-your-nodejs-config-file
  */
 exports.config = {
     app_name: ['Google+ User Feed'],
     license_key: '020f82ad80dfc3b5139dd94734b421c4fce5c673',
     logging: {
-        level: 'trace'
+        level: 'info'
     }
 };
