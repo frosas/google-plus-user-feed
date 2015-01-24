07a822a85e8feb00442076ea30a8576c6f33c5e8
Francesc Rosàs <francescrosasbosque@gmail.com>
2015-01-24 01:53:23 +0000

    Add ESLint

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
-'use strict'
+'use strict';
 
-var App = require('./App')
-var GooglePlus = require('./GooglePlus')
-var CachedUserItems = require('./GooglePlus/CachedUserItems')
+var App = require('./App');
+var GooglePlus = require('./GooglePlus');
+var CachedUserItems = require('./GooglePlus/CachedUserItems');
 
-var googlePlus = new GooglePlus(process.env.GOOGLE_API_KEY)
-var cachedUserItems = new CachedUserItems(googlePlus)
+var googlePlus = new GooglePlus(process.env.GOOGLE_API_KEY);
+var cachedUserItems = new CachedUserItems(googlePlus);
 
-new App(cachedUserItems).listen(process.env.PORT || 8080)
+new App(cachedUserItems).listen(process.env.PORT || 8080);
