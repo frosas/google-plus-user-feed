a513ba102ec36c9c6b08f609897d437e69e00581
Francesc Rosàs <francescrosasbosque@gmail.com>
2015-04-23 22:17:06 +0100

    Update newrelic.js

--- newrelic.js
+++ newrelic.js
@@ -7,6 +7,7 @@
  */
 exports.config = {
     app_name: ['Google+ User Feed'],
-    license_key: '020f82ad80dfc3b5139dd94734b421c4fce5c673',
-    logging: {level: 'info'}
+    capture_params: true,
+    logging: {level: 'warn'},
+    ignore_status_codes: [400, 401, 402, 403, 404, 405, 406, 407, 408, 409, 410, 411, 412, 413, 414, 415, 416, 417]
 };
