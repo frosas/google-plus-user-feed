db00719fa29ada2af6e9962ea3af185400356471
Francesc Rosas <francescrosasbosque@gmail.com>
2017-10-22 15:23:05 +0200

    Refact

--- src/server.js
+++ src/server.js
@@ -10,9 +10,7 @@ const CachedFeedsRepository = require('./cached-feeds-repository');
 process.on('unhandledRejection', error => { throw error; });
 
 CachedFeedsRepository.create('persistent/main.db').then(repository => {
-    const feeds = new CachedFeeds({
-        googlePlus: new GooglePlus(process.env.GOOGLE_API_KEY),
-        repository
-    });
+    const googlePlus = new GooglePlus(process.env.GOOGLE_API_KEY);
+    const feeds = new CachedFeeds({googlePlus, repository});
     new App(feeds).listen(process.env.PORT || 8080);
 });
